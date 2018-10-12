61caeb8606b7cbced267b87c57d5d7a40b924e5e
alpert <alper.tekinalp@gmail.com>
2018-10-12 14:51:21 +0300

    Remove and ignore target

--- target/site/cucumber-report/report.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Update.feature");
-formatter.feature({
-  "line": 1,
-  "name": "UpdateFeature",
-  "description": "This feature deals with the update personal information and password functionality of the application",
-  "id": "updatefeature",
-  "keyword": "Feature"
-});
