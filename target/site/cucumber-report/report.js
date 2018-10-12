937a8eb820a8e521a8e6acac50c3a20b13b75c9d
emrah-bakirtas <emrah.bakirtas@ge.com>
2018-10-12 13:08:55 +0300

    minor things

---
+++ target/site/cucumber-report/report.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Update.feature");
+formatter.feature({
+  "line": 1,
+  "name": "UpdateFeature",
+  "description": "This feature deals with the update personal information and password functionality of the application",
+  "id": "updatefeature",
+  "keyword": "Feature"
+});
